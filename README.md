52c996612353afadcca2504ba925fd12a9647e59
wokeoke <woke.oke@gmail.com>
2022-05-05 09:04:33 +0700

    Select Elements

--- README.md
+++ README.md
@@ -63,3 +63,21 @@
   </body>
 </html>
 ```
+
+- #### Select Elements
+
+```js
+// SELECT ITEMS
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+// EDIT OPTION
+let editElement;
+let editFlag = false;
+let editId = '';
+```
--- grocery-bud/app.js
+++ grocery-bud/app.js
@@ -1,11 +1,21 @@
-// ****** SELECT ITEMS **********
+// SELECT ITEMS
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
-// edit option
+// EDIT OPTION
+let editElement;
+let editFlag = false;
+let editId = '';
 
-// ****** EVENT LISTENERS **********
+// EVENT LISTENERS
 
-// ****** FUNCTIONS **********
+// FUNCTIONS
 
-// ****** LOCAL STORAGE **********
+// LOCAL STORAGE
 
-// ****** SETUP ITEMS **********
+// SETUP ITEMS
